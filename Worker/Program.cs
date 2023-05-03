50dc583f4c42961905abfb355ca9b53ef849067e
Fabio Cavalcante <facaval@microsoft.com>
2023-05-02 19:33:48 -0700

    Cleaning up DS load repro

--- Worker/Program.cs
+++ Worker/Program.cs
@@ -1,11 +1,4 @@
-﻿Console.WriteLine(TestDep.TestClass.GetTypeName());
-
-Console.WriteLine("Press enter to run load type with dependency on System.Diagnosics.DiagnosticSource 7.0 ...");
+﻿Console.WriteLine("Press enter to load System.Diagnosics.DiagnosticSource 7.0 ...");
 Console.ReadLine();
 
-Console.WriteLine(TestDep70.TestClass2.GetTypeName());
-
-//var assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(@"C:\Users\facaval\source\repos\Loader\Worker\bin\System.Diagnostics.DiagnosticSource.dll");
-//var assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(@"C:\Program Files\dotnet\shared\Microsoft.NETCore.App\7.0.5\System.Diagnostics.DiagnosticSource.dll");
-
-//Console.WriteLine(assembly.FullName);
+Console.WriteLine(typeof(System.Diagnostics.Activity).AssemblyQualifiedName);
